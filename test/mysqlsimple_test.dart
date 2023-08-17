f36769508e728af65f940b63c747340cde314941
HeNew <angelmaclovio@yahoo.com>
2023-08-17 15:06:22 -0600

    TODO Test

--- test/mysqlsimple_test.dart
+++ test/mysqlsimple_test.dart
@@ -1,6 +1,8 @@
 import 'package:mysqlsimple/mysqlsimple.dart';
 import 'package:test/test.dart';
 
+/// TODO: Add test :D
+
 void main() {
   group('A group of tests', () {
     final awesome = Awesome();
